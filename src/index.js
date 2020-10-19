76f3c201d650c96498f208856b120b827dd4f8ea
aimee-liang <aimeeeliang@gmail.com>
2020-10-19 17:54:40 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+/* get and render information */
+
+/* User should click on a category. On click, Question and answers will pop up */
+
+/* User can click next button to go on to the next question */
+
+/* User should be able to see their score */
+
+})
