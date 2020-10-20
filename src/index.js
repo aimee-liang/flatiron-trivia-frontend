d2ae3009eacc9af73c77fc06bfe677575eb98d94
github username <github email address>
2020-10-20 18:45:34 -0400

    need help

--- src/index.js
+++ src/index.js
@@ -1,50 +1,80 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const categoryUrl = "http://localhost:3000/categories/"
+    
     const questionUrl = "http://localhost:3000/questions/"
 
 /* get and render categories */
-    const getCategories = () => {
-        fetch(categoryUrl)
-            .then(response => response.json())
-            .then(categories => {
-                renderCategories(categories)
-            })
-    }
-
-    const renderCategories = categories => {
-        for (const category of categories){
-            renderCategory(category)
-        }
-    }
-
-    const renderCategory = category => {
-        const categoryBar = document.querySelector("#category-bar")
-        const categorySpan = document.createElement("span")
-        categorySpan.dataset.id = `${category.id}`
-        categorySpan.textContent = `${category.title}`
-
-        categoryBar.append(categorySpan)
-    }
-
-/* User should click on a category. On click, Question and answers will pop up */
-    const spanClick = () => {
-        document.addEventListener("click", e => {
-            if (e.target.matches("span")){
-                const categoryId = e.target.dataset.id
-                console.log(e)
-                fetch(categoryUrl + categoryId)
-                    .then(response => response.json())
-                    .then(getQuestions)
-            }
-        })
-    }
-    
-/* get and render questions */
     const getQuestions = () => {
         fetch(questionUrl)
             .then(response => response.json())
-            .then(questions => console.log(questions))
+            .then(questions => {
+                renderQuestions(questions)
+            })
     }
+
+    const renderQuestions = questions => {
+        // console.log(questions[3].text)
+        for (const question of questions){
+            // renderText(question)
+        const questionPanel = document.querySelector("#question-panel")
+        const h4 = document.createElement('h4')
+        h4.dataset.questionId = `${question.id}`
+        h4.textContent = `${question.text}`
+        questionPanel.append(h4)
+        }
+    }
+
+    const clickText = () => {
+        document.addEventListener('click', (e) => {
+            if(e.target.matches('h4')){
+                const questionId = e.target.dataset.questionId
+                fetch(questionUrl + questionId)
+                .then(response => response.json())
+                .then(textInfo)
+            }
+        const textInfo = () => {
+            
+
+        }
+
+
+        })
+    }
+
+ 
+
+    // const renderText = question => {
+    //     const questionPanel = document.querySelector("#question-panel")
+    //     // console.log(questionPanel)
+    //     const h2 = document.createElement('h2')
+    //     h2.textContent = `${question.text}`
+    //     console.log(h2)
+
+        // const questionDiv = document.createElement("span")
+        // questionDiv.dataset.id = `${question.id}`
+        // questionDiv.textContent = `${question.title}`
+        // console.log(question)
+        // categoryBar.append(questionDiv)
+    // }
+
+/* User should click on a category. On click, Question and answers will pop up */
+    // const spanClick = () => {
+    //     document.addEventListener("click", e => {
+    //         if (e.target.matches("span")){
+    //             const categoryId = e.target.dataset.id
+    //             console.log(e)
+    //             fetch(categoryUrl + categoryId)
+    //                 .then(response => response.json())
+    //                 .then(getQuestions)
+    //         }
+    //     })
+    // }
+    
+/* get and render questions */
+    // const getQuestions = () => {
+    //     fetch(questionUrl)
+    //         .then(response => response.json())
+    //         .then(questions => console.log(questions))
+    // }
 
     // const renderQuestions = questions => {
     //     const questionPanel = document.querySelector("#question-panel")
@@ -71,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /* User should be able to see their score */
 
-    getCategories();
-    spanClick();
+   
+    clickText()
     getQuestions();
 })
