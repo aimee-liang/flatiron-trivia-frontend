de9fa2a6f849a850332e1a2c183c2b0b9b133cae
aimee-liang <aimeeeliang@gmail.com>
2020-10-22 16:57:33 -0400

    Most up to date one

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const questionUrl = "http://localhost:3000/questions/"
     const gameUrl = "http://localhost:3000/games/"
-    
-    
-  
-
-    // console.log(typeof txtContent)
-    // console.log(typeof pointsSpan)
-    
+        
 /* get and render categories */
 const clickStartGame = () => {
     document.addEventListener('click', (e) => {
         if(e.target.matches(("#start-game"))){
-            const questionArray = [243, 244, 245, 246, 247];
+            const questionArray = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70];
             let questionId = questionArray[Math.floor(Math.random()*questionArray.length)]
 
             fetch(questionUrl + questionId)
@@ -23,30 +17,20 @@ const clickStartGame = () => {
                 // set time out
                 const correctContainer = document.querySelector(".container")
                 correctContainer.textContent = "Correct!"
-                correctContainer.style.display = "block"
-
-                // const pointsSpan = document.querySelector("#points")
-                // pointsSpan = + 5
+                // correctContainer.style.display = "block"
             }
 
             const displayIncorrect = () => {
                 // set time out
                 const incorrectContainer = document.querySelector(".container")
                 incorrectContainer.textContent = "INCORRECT!"
-                incorrectContainer.style.display = "block"
-
-                // const pointsSpan = document.querySelector("#points")
-                // pointsSpan = + 0
+                // incorrectContainer.style.display = "block"
             }
 
             const nextQuestion = () => {
             // need a way to indicate next
-                // const nextButton = document.createElement("button")
-                // nextButton.textContent = "Next Question"
-                // const nextClass = document.querySelector("#next")
-                // nextClass.append(nextButton)
-
-                const questionArray = [243, 244, 245, 246, 247];
+                
+                const questionArray = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70];
                 let questionId = questionArray[Math.floor(Math.random()*questionArray.length)]
                 
             // clear out the HTML in the question and answer panels
@@ -55,14 +39,11 @@ const clickStartGame = () => {
 
                 const questionPanel = document.querySelector("#question-panel")
                 questionPanel.innerHTML = ''
-
-                // nextClass.innerHTML = ''
                 
             fetch(questionUrl + questionId)
                 .then(response => response.json())
                 .then(question => displayQuestion(question))
             }
-
 
             const answerClicker = (question) => {
                 const answerPanel = document.querySelector("#answer-panel")
@@ -72,17 +53,17 @@ const clickStartGame = () => {
                         const answerPanel = document.querySelector("#answer-panel")
                 answerPanel.innerHTML = ''
                         displayCorrect()                       
-                        setTimeout(nextQuestion, 1000)
+                        setTimeout(nextQuestion, 1000)  //this method will fetch a new question
                         increaseScore()
-                         //this method will fetch a new question
                     } else {
                         displayIncorrect()
                         setTimeout(nextQuestion, 1000)
                     }
                 })
             }
+
             function shuffle(a) {
-                var j, x, i;
+                let j, x, i;
                 for (i = a.length - 1; i > 0; i--) {
                     j = Math.floor(Math.random() * (i + 1));
                     x = a[i];
@@ -91,6 +72,7 @@ const clickStartGame = () => {
                 }
                 return a;
             }
+
             //this method will create buttons for each answer 
             const displayAnswers = (question) => {
                 const answerPanel = document.querySelector("#answer-panel")
@@ -98,37 +80,51 @@ const clickStartGame = () => {
                 let totalAnswers = question.incorrect_answers
                 totalAnswers.push(question.answer)
                 shuffle(totalAnswers)
+                answerPanel.innerHTML = ''
                     // TO DO: implement shuffling in total answers
-
                 totalAnswers.forEach(answer => {
                     const answerButton = document.createElement("button") //for each string element in array, create a new button
                     answerButton.textContent = answer// the text content should be an answer
                     answerButton.dataset.id = question.id
                     answerPanel.append(answerButton)  //that will be appended to the answer panel
                 })
-
                 answerClicker(question)
             }
-
-            const displayQuestion = (question) => {
-                // const pointsSpan = 0
-                // // console.log(typeof pointsSpan)
-                
             
-                const questionPanel = document.querySelector("#question-panel")
-                questionPanel.innerHTML = `
-                <h2>${question.text}</h2>
-                `
-                displayAnswers(question)
+            const countDown = () => {
                 
+                // let interval = setInterval(function(){
+                  let timer = document.getElementById('count').innerHTML=timer;
+                  timer--;
+                //   console.log(timer)
+                if (timer === 0){
+                    // clearInterval(interval);
+                    document.getElementById('count').innerHTML='Done';
+                    // or...
+                    alert("You're out of time!");
+                  }
+                // }, 1000);
+
+            }
+            
+
+            const displayQuestion = (question) => {                
+                const questionPanel = document.querySelector("#question-panel")
+                // questionPanel.innerHTML = `<h2>${question.text}</h2>`
+                const h2 = document.createElement("h2")
+                h2.textContent = `${question.text}`
+                h2.classList.add("h2-question")
+                questionPanel.append(h2)
+                displayAnswers(question)
+                countDown()
             }
             
             const increaseScore = () => {
                 let points = document.querySelector("#points")
                 let txtContent = points.textContent
+                
                 const pointsSpan = parseInt(txtContent)
-                console.log(pointsSpan)
-               
+                
                 const options = {
                     method: "PATCH",
                     headers: {
@@ -138,11 +134,10 @@ const clickStartGame = () => {
                     body: JSON.stringify({score: pointsSpan + 5})
                 }
 
-                fetch(gameUrl + 26, options)
+                fetch(gameUrl + 7, options)
                 .then(response => response.json())
                 .then(data => {
                     points.textContent = data.score
-                    console.log(txtContent)
                 })
 
             }
