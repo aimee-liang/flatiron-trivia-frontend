4b85958c75eb57f8467bae71aecf9e1cb2398805
aimee-liang <aimeeeliang@gmail.com>
2020-10-22 23:08:47 -0400

    Included README, updated CSS styling, an endGame function, and new elements to HTML

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# flatiron-trivia-frontend
+# Flatiron Trivia
+
+Welcome to our Flatiron Trivia Game for Mod3. Bryam and I wanted to create an interactive web application where users would be able to select an answer based on the question generated. Their points would persist on the page. If correct, a block of text would alert the user they are correct. If incorrect or if the user runs out the clock, they would lose. 
+
+Due to the nature of the API we were hoping to use, we had to seed our information into the backend, which makes our code less flexible than we hoped. Stretch features would've included a top scores board, a timer function, a points span, more animations for the alerts, more interactive features, 
+
+Credits to OpenTDB for the API information we seeded.
+
+# Product of Bryam Vicente and Aimee Liang
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const questionUrl = "http://localhost:3000/questions/"
     const gameUrl = "http://localhost:3000/games/"
+    const usersUrl = "http://localhost:3000/users/"
         
 /* get and render categories */
 const clickStartGame = () => {
     document.addEventListener('click', (e) => {
         if(e.target.matches(("#start-game"))){
-            const questionArray = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70];
+            const questionArray = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79];
             let questionId = questionArray[Math.floor(Math.random()*questionArray.length)]
 
             fetch(questionUrl + questionId)
@@ -16,21 +17,19 @@ const clickStartGame = () => {
             const displayCorrect = () => {
                 // set time out
                 const correctContainer = document.querySelector(".container")
-                correctContainer.textContent = "Correct!"
-                // correctContainer.style.display = "block"
+                correctContainer.textContent = "CORRECT!"
             }
 
             const displayIncorrect = () => {
                 // set time out
                 const incorrectContainer = document.querySelector(".container")
                 incorrectContainer.textContent = "INCORRECT!"
-                // incorrectContainer.style.display = "block"
             }
 
             const nextQuestion = () => {
             // need a way to indicate next
                 
-                const questionArray = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70];
+                const questionArray = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79];
                 let questionId = questionArray[Math.floor(Math.random()*questionArray.length)]
                 
             // clear out the HTML in the question and answer panels
@@ -45,19 +44,40 @@ const clickStartGame = () => {
                 .then(question => displayQuestion(question))
             }
 
+            const endGame = () => {
+                const questionPanel = document.querySelector("#question-panel")
+                const h2 = document.createElement("h2")
+
+                h2.textContent = `Thank you for playing! See you next time, loser.`
+
+                h2.classList.add("h2-question")
+                questionPanel.innerHTML = ''
+                questionPanel.append(h2)
+
+                const answerPanel = document.querySelector("#answer-panel")
+                answerPanel.innerHTML = ''
+
+                const points = document.querySelector("#points")
+                points.textContent = ''
+
+                const startGameButton = document.querySelector("#start-game")
+                startGameButton.textContent = `Start a new game`
+            }
+
             const answerClicker = (question) => {
                 const answerPanel = document.querySelector("#answer-panel")
                 answerPanel.addEventListener("click", e => {
                     const button = e.target
                     if (button.textContent === question.answer){
                         const answerPanel = document.querySelector("#answer-panel")
-                answerPanel.innerHTML = ''
+                        answerPanel.innerHTML = ''
                         displayCorrect()                       
                         setTimeout(nextQuestion, 1000)  //this method will fetch a new question
                         increaseScore()
                     } else {
                         displayIncorrect()
-                        setTimeout(nextQuestion, 1000)
+                        // setTimeout(nextQuestion, 1000)
+                        endGame()
                     }
                 })
             }
@@ -72,7 +92,7 @@ const clickStartGame = () => {
                 }
                 return a;
             }
-
+            
             //this method will create buttons for each answer 
             const displayAnswers = (question) => {
                 const answerPanel = document.querySelector("#answer-panel")
@@ -86,37 +106,39 @@ const clickStartGame = () => {
                     const answerButton = document.createElement("button") //for each string element in array, create a new button
                     answerButton.textContent = answer// the text content should be an answer
                     answerButton.dataset.id = question.id
+                    answerButton.classList.add("answerBtn")
                     answerPanel.append(answerButton)  //that will be appended to the answer panel
                 })
                 answerClicker(question)
             }
-            
-            const countDown = () => {
+
+            // const countDown = () => {
                 
-                // let interval = setInterval(function(){
-                  let timer = document.getElementById('count').innerHTML=timer;
-                  timer--;
-                //   console.log(timer)
-                if (timer === 0){
-                    // clearInterval(interval);
-                    document.getElementById('count').innerHTML='Done';
-                    // or...
-                    alert("You're out of time!");
-                  }
-                // }, 1000);
+            //     // let interval = setInterval(function(){
+            //       let timer = document.getElementById('count').innerHTML=timer;
+            //       timer--;
+            //     //   console.log(timer)
+            //     if (timer === 0){
+            //         // clearInterval(interval);
+            //         document.getElementById('count').innerHTML='Done';
+            //         // or...
+            //         alert("You're out of time!");
+            //       }
+            //     // }, 1000);
 
-            }
+            // }
             
-
             const displayQuestion = (question) => {                
                 const questionPanel = document.querySelector("#question-panel")
-                // questionPanel.innerHTML = `<h2>${question.text}</h2>`
+
                 const h2 = document.createElement("h2")
                 h2.textContent = `${question.text}`
                 h2.classList.add("h2-question")
+                questionPanel.innerHTML = ''
                 questionPanel.append(h2)
+
                 displayAnswers(question)
-                countDown()
+                // countDown()
             }
             
             const increaseScore = () => {
@@ -133,24 +155,18 @@ const clickStartGame = () => {
                     },
                     body: JSON.stringify({score: pointsSpan + 5})
                 }
-
+                
+/* User should be able to see their score */
                 fetch(gameUrl + 7, options)
                 .then(response => response.json())
                 .then(data => {
                     points.textContent = data.score
                 })
-
             }
-
-            
-
-
         }    
-    
     })
 }
 
-/* User should be able to see their score */
 
 
 clickStartGame()
